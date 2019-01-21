c450963e75b87c1f2e69b90be5ee06705a4b2400
becky henry <becky8135@gmail.com>
2019-01-20 23:43:43 -0700

    First Draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+/*select a random number at the start of the game. 
+
+There are 4 crystals below. By clicking on a crystal, you will add a specific amount of points to your total score
+
+You win the game by matching your total score to random number, you lose the game if your total score goes above this random number.
+
+The value of each crystal is hidden from you until you click on it
+
+each time when the game starts the game will change the values of each crystal */
+
+// generate 4 random numbers for the 4 crystals
+// return 4 numbers
+
+// var playerScore = 0;
+// var targetScore = Math.floor((Math.random() * 120));
+// const crystals = [{
+//         src: "assets/images/red1.jpg",
+//         value: generateCrystalNumber(),
+//         name: "red"
+//     },
+//     {
+//         src: "assets/images/blue2.jpg",
+//         value: generateCrystalNumber(),
+//         name: "green"
+//     },
+//     {
+//         src: "assets/images/yellow.jpg",
+//         value: generateCrystalNumber(),
+//         name: "yellow"
+//     },
+//     {
+//         src: "assets/images/green1.jpg",
+//         value: generateCrystalNumber(),
+//         name: "green"
+//     }
+// ]
+
+// $(document).ready(function () {
+// startGame()
+
+// });
+// console.log('startGame');
+
+// function startGame(){
+//     for (var i = 0; i < crystals.length; i++){
+        
+//         var crystalButton = $('<img>')
+//         crystalButton.attr('src', crystals[i].src)
+//         crystalButton.attr('value', crystals[i].value)
+//         $('#crystals').append(crystalButton)   
+//     }
+// };
+// $('#crystals').on('click','img', function () {
+
+    
+// })
+// function generateCrystalNumber() {
+
+//     return Math.floor(Math.random() * 12);
+// }
+
+
+
+
